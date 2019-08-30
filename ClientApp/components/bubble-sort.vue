0398eff38717e678c778b60805e989fb0ddea406
Karam Kabbara <contact@karam.io>
2019-08-30 21:01:04 +0100

    Added feedback modals with scrolling.

--- ClientApp/components/bubble-sort.vue
+++ ClientApp/components/bubble-sort.vue
@@ -1,5 +1,30 @@
 <template>
   <div class="algorithm-container">
+    <div class="notification is-success" v-show="showSuccess">
+      <button class="delete" @click="closeModal()"></button>
+      <strong>Well Done!</strong>
+      <br />You have ordered
+      <strong>Bubble Sort</strong> correctly!
+    </div>
+
+    <div class="notification is-danger" v-show="showFailure">
+      <button class="delete" @click="closeModal()"></button>
+      <strong>Oh No!</strong>
+      <br />You have ordered
+      <strong>Bubble Sort</strong> incorrectly! Please try again.
+    </div>
+
+    <div class="notification is-warning" v-show="showHelp">
+      <button class="delete" @click="closeModal()"></button>
+      <strong>Need some help?</strong>
+      <br />
+      <ul>
+      <li>Starting with an unsorted list, iterate through each element and compare it to the element next to it.</li>
+      <li>If the element, is greater than the element next to it, they are swapped.</li>
+      <li>This process is then repeated until we reach the end of the list. If the list is still unsorted, the Bubble Sort starts again from the start of the list.</li>
+      </ul>
+    </div>
+
     <draggable v-model="myArray" group="people" @start="drag=true" @end="drag=false">
       <div class="box" v-for="element in myArray" :key="element.id" v-html="element.text"></div>
     </draggable>
@@ -12,9 +37,11 @@
         </span>
         <span>Done!</span>
       </a>
+
       &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
       <a
         class="button is-warning is-large is-rounded"
+        @click="help()"
       >
         <span class="icon is-large">
           <i class="fas fa-question-circle"></i>
@@ -40,6 +67,9 @@ export default {
 
   data() {
     return {
+      showSuccess: false,
+      showFailure: false,
+      showHelp: false,
       myArray: [
         { id: 1, text: "swapped = <strong>false</strong>" },
         {
@@ -97,9 +127,34 @@ export default {
       }
     },
 
+    scrollToTop() {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      });
+    },
+
+    closeModal() {
+      this.showSuccess = false;
+      this.showFailure = false;
+      this.showHelp = false;
+    },
+
     done() {
-      if (JSON.stringify(this.myArray) === JSON.stringify(this.correctArray)) return alert("Well done!")
-      else return alert("Oh no, try again!");
+      if (JSON.stringify(this.myArray) === JSON.stringify(this.correctArray)) {
+        this.showSuccess = true;
+        this.showFailure = false;
+        this.scrollToTop();
+      } else {
+        this.showSuccess = false;
+        this.showFailure = true;
+        this.scrollToTop();
+      }
+    },
+
+    help() {
+      this.showHelp = true;
+      this.scrollToTop();
     }
   }
 };
