7d10f75a84e112c6d138343c1f97903e25111537
Karam Kabbara <contact@karam.io>
2019-09-01 12:02:36 +0100

    Trying to get styles/serviceworker working on aws.

--- ClientApp/router/routes.js
+++ ClientApp/router/routes.js
@@ -3,5 +3,6 @@ import BubbleSort from 'components/bubble-sort'
 
 export const routes = [
   { name: 'home', path: '/', component: HomePage, display: 'Home', icon: 'home' },
+  { name: 'home', path: '/home', component: HomePage, display: 'Home', icon: 'home' },
   { name: 'bubblesort', path: '/bubblesort', component: BubbleSort }
 ]
